7fbfdbea792430359dc3b237dd05b3c7d30e9713
hlin <huangl@seaskylight.com>
2017-12-28 10:39:19 +0800

    项目初始化

---
+++ oauthclient/src/main/java/com/demo/oauth2/OauthclientApplication.java
@@ -0,0 +1,12 @@
+package com.demo.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthclientApplication.class, args);
+	}
+}
